0e519695fda047dfff20a7caa18f00d906165764
DaniLEP <danilomanzoli64@gmail..com>
2024-09-18 19:55:13 -0300

    fix: modify path images

--- src/components/OrdersPage.jsx
+++ src/components/OrdersPage.jsx
@@ -19,12 +19,12 @@ export default function OrdersPage() {
 
       <div onClick={goToEditOrder} style={{maxWidth: "240px", height: "190px", margin: "0 auto", position:"relative", top:"23vh", right: "35vh", backgroundColor: "#fff", padding: "20px", borderRadius: "8px", color: "black", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)"}}>
         {/* Ao clicar, redireciona para a página de edição do pedido */}
-        <img src="/public/mensal.svg" className='w-80 h-[130px] align-center'alt="" />
+        <img src="//mensal.svg" className='w-80 h-[130px] align-center'alt="" />
         <h2>Pedido do Mês Anterior</h2>
       </div>
 
       <div onClick={goToShoppingList} style={{maxWidth: "240px", height: "190px", margin: "0 auto", position:"relative", top: "-7vh", right:"-40vh", backgroundColor: "#fff", padding: "20px", borderRadius: "8px", color: "black", boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)"}}>
-        <img src='/public/plus.svg' className='w-60 h-20 align-center' alt='Novo Pedido' />
+        <img src='//plus.svg' className='w-60 h-20 align-center' alt='Novo Pedido' />
         <br />
         <h2>Fazer Novo Pedido</h2>
       </div>
--- src/pages/Cadastro.jsx
+++ src/pages/Cadastro.jsx
@@ -21,33 +21,33 @@ export default function Cadastro  ()  {
                                    color: "black"
                                 }}>
                                 <Link  spy={true} smooth={true} to={"/Proteina"} onclick="navigateTo('cadastro-proteina')">
-                                    <img src="/public/proteinas.svg" className="ml-11 h-[120px]"/>
+                                    <img src="//proteinas.svg" className="ml-11 h-[120px]"/>
                                         <h2 className="text-[40px]">Proteínas</h2>
                                 </Link>
                             </div>
 
                             <div className="card" style={{ background: "#fff", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0,0,0,0.1)", padding: "20px", cursor: "pointer", transition: "transform 0.3s ease", fontSize: "2rem", marginBottom: "10px", textDecoration: "bold", color: "black"}}>
                                 <Link spy={true} smooth={true}  to={"/Mantimento"} onclick="navigateTo('cadastro-mantimentos')">
-                                    <img src="/public/mantimentos.svg" className=" h-[120px]"/>
+                                    <img src="//mantimentos.svg" className=" h-[120px]"/>
                                         <h2 className="text-[40px]">Mantimentos</h2>
                                 </Link>
                             </div>
 
                             <div className="card" style={{ background: "#fff", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0,0,0,0.1)", padding: "20px", cursor: "pointer", transition: "transform 0.3s ease", fontSize: "2rem", marginBottom: "10px", textDecoration: "bold", color: "black"}}>
                                 <Link spy={true} smooth={true} to={"/Hortalicas"} onclick="navigateTo('Hortalicas')">
-                                    <img src="/public/hortalicas.svg" className="h-[120px] ml-5"/>
+                                    <img src="//hortalicas.svg" className="h-[120px] ml-5"/>
                                         <h2 className="text-[40px]">Hortaliças</h2>
                                 </Link>
                             </div>
                             <div className="card" style={{ background: "#fff", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0,0,0,0.1)", padding: "20px", cursor: "pointer", transition: "transform 0.3s ease", fontSize: "2rem", marginBottom: "10px", textDecoration: "bold", color: "black"}}>
                                 <Link spy={true} smooth={true} to={"/Cad.Refeicoes"} onclick="navigateTo('Ref.Servidas')">
-                                    <img src="/public/ref.servidas.svg" className="h-[120px] ml-14"/>
+                                    <img src="//ref.servidas.svg" className="h-[120px] ml-14"/>
                                         <h2 className="text-[40px]">Ref.Servidas</h2>
                                 </Link>
                             </div>
                             <div className="card"  style={{ background: "#fff", borderRadius: "8px", boxShadow: "0 4px 8px rgba(0,0,0,0.1)", padding: "20px", cursor: "pointer", transition: "transform 0.3s ease", fontSize: "1.5rem", marginBottom: "10px", textDecoration: "none", color: "black"}}>
                                 <Link spy={true} smooth={true} to={"/"} onclick="navigateTo('Home')">
-                                    <img src="/public/return.svg" className="h-[120px] ml-16 mt-5"/>
+                                    <img src="//return.svg" className="h-[120px] ml-16 mt-5"/>
                                 </Link>
                             </div>
                          
