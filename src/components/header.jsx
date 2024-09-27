95ef77c20fd16b321fa2efd3a49590ed32a039a6
DaniLEP <danilomanzoli64@gmail..com>
2024-09-27 11:38:14 -0300

    ft: alteration the home page, header and cards

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,67 +1,3 @@
-// import  { useState } from "react";
-// import {FaTimes} from "react-icons/fa";
-// import { CiMenuFries } from "react-icons/ci";
-// import { Link } from "react-router-dom";
-
-
-// export default function Header  ()  {
-//     const [click, setClick] = useState(false);
-//     const handleClick = () => setClick(!click);
-//     const content = () => <>
-//     <div className="lg:hidden block relative top-16 w-full left-0 right-0 background-[00009c] transition">
-//         <ul className="text-center text-white text-xl p-20">
-//             <Link spy={true} smooth={true}  to="/ListaPedidos">
-//                   <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:rounded">Lista de Pedidos</li>
-//             </Link>
-//             <Link  spy={true} smooth={true} to="/Cadastro">
-//                   <li className="my-4 py-4 border-b border-[#00FF62]  border-white hover:bg-white hover:rounded">Cadastr0</li>
-//             </Link>
-//             <Link spy={true} smooth={true} to="/Relatorio">
-//                   <li className="my-4 py-4 border-b border-[#00FF62]  border-white hover:bg-white hover:rounded">Relatório</li>
-//             </Link>
-//             <Link  spy={true} smooth={true} to="/Retirada">
-//                   <li className="my-4 py-4 border-b border-[#00FF62]  border-white hover:bg-white hover:rounded">Retirada</li>
-//             </Link>
-//         </ul>
-//     </div>
-//     </>
-//     return(  
-//         <nav>
-//             <div className="flex h-[80px] w-full items-center justify-between py-[20px] px-[96px] border-b">
-//             <div className="flex items-center flex-1">
-//             <span style={{width: "150px", height: "105px", position: "relative", top: "2vh", right: "1vh"}}>
-//                 <img src="/logo.svg" alt="Logo-Instituto-Reciclar" />
-//             </span>
-//             </div>
-//             <div className="lg:flex md:flex  lg: flex-1 items center justify-end text-white !font-['Chakra Petch, sans serif'] hidden">
-//                 <div className="flex-10 ">
-//                     <ul className="flex gap-2 mr-16 mt-[-7px] text-[20px] ">
-//                         <Link spy={true} smooth={true} to="/lista-pedidos">
-//                             <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal  border !border-none h-[48px] px-4 py-2">Lista de Pedidos</button>
-//                         </Link>
-//                         <Link spy={true} smooth={true} to="/cadastro">
-//                         <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal  border !border-none h-[48px] px-4 py-2">Cadastro</button>
-//                         </Link>
-//                         <Link spy={true} smooth={true} to="/relatorio">
-//                              <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal  border !border-none h-[48px] px-4 py-2">Relatório</button>
-//                         </Link>
-//                         <Link spy={true} smooth={true} to="/retirada">
-//                              <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal  border !border-none h-[48px] px-4 py-2">Retirada</button>
-//                         </Link>
-//                     </ul>                        
-//                 </div>
-//             </div>
-//                 <div>
-//                     {click && content}
-//                 </div>
-//                 <button className="block sm:hidden trasition block color-white text-white text-[30px]" onClick={handleClick}>
-//                     {click ? <FaTimes/> : <CiMenuFries/>}
-//                 </button>
-//         </div>  
-//     </nav>
-//     )
-// }
-
 import { useState } from "react";
 import { FaTimes } from "react-icons/fa";
 import { CiMenuFries } from "react-icons/ci";
@@ -72,79 +8,183 @@ export default function Header() {
   const handleClick = () => setClick(!click);
 
   return (
-    <nav>
-      <div className="flex h-[80px] w-full items-center justify-between py-[20px] px-[16px] lg:px-[96px] border-b">
-        {/* Logo */}
-        <div className="flex items-center flex-1">
-          <span className="w-[150px] h-[105px] mt-5">
-            <img src="/logo.svg" alt="Logo-Instituto-Reciclar" />
-          </span>
-        </div>
+    <>
+      <nav>
+        <div className="flex h-[77px] w-full items-center justify-between py-[20px] px-[96px] border-b">
+          {/* Logo */}
+          <div className="flex items-center flex-1">
+            <span className="w-[235px] ">
+              <img src="/logo.svg" alt="Logo-Instituto-Reciclar" />
+            </span>
+          </div>
 
-        {/* Menu (Desktop) */}
-        <div className="hidden lg:flex md:flex lg:flex-1 items-center justify-end text-white !font-['Chakra Petch, sans serif']">
-          <ul className="flex gap-2 mr-16 mt-[-7px] text-[20px] ">
-            <Link spy={true} smooth={true} to="/lista-pedidos">
-              <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal border !border-none h-[48px] px-4 py-2">
-                Lista de Pedidos
-              </button>
-            </Link>
-            <Link spy={true} smooth={true} to="/cadastro">
-              <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal border !border-none h-[48px] px-4 py-2">
-                Cadastro
-              </button>
-            </Link>
-            <Link spy={true} smooth={true} to="/relatorio">
-              <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal border !border-none h-[48px] px-4 py-2">
-                Relatório
-              </button>
-            </Link>
-            <Link spy={true} smooth={true} to="/retirada">
-              <button className="inline-flex items-center !text-[#09090B] justify-center whitespace-nowrap !rounded-none text-[16px] font-bold transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 !text-white underline-offset-4 hover:!text-primary !font-normal border !border-none h-[48px] px-4 py-2">
-                Retirada
-              </button>
-            </Link>
-          </ul>
-        </div>
+          {/* Menu (Desktop) */}
+          <div className="hidden lg:flex flex-1 items-center justify-end text-white">
+            <ul className="flex gap-2 mr-16 mt-[-7px] text-[20px]">
+              <Link to="/lista-pedidos">
+                <button className="inline-flex items-center text-white justify-center text-[16px] font-bold transition-colors focus-visible:outline-none hover:text-primary h-[48px] px-4 py-2">
+                  Lista de Pedidos
+                </button>
+              </Link>
+              <Link to="/cadastro">
+                <button className="inline-flex items-center text-white justify-center text-[16px] font-bold transition-colors focus-visible:outline-none hover:text-primary h-[48px] px-4 py-2">
+                  Cadastro
+                </button>
+              </Link>
+              <Link to="/relatorio">
+                <button className="inline-flex items-center text-white justify-center text-[16px] font-bold transition-colors focus-visible:outline-none hover:text-primary h-[48px] px-4 py-2">
+                  Relatório
+                </button>
+              </Link>
+              <Link to="/retirada">
+                <button className="inline-flex items-center text-white justify-center text-[16px] font-bold transition-colors focus-visible:outline-none hover:text-primary h-[48px] px-4 py-2">
+                  Retirada
+                </button>
+              </Link>
+            </ul>
+          </div>
 
-        {/* Menu (Mobile) */}
-        <div className="lg:hidden block relative">
-          {click && (
-            <div className="absolute top-16 left-0 w-full bg-[#00009c] transition-all duration-300 ease-in-out">
-              <ul className="text-center text-white text-xl p-20">
+          {/* Botão do Menu (Mobile) */}
+          <button
+            className="block lg:hidden text-white text-[30px] z-50"
+            onClick={handleClick}
+          >
+            {click ? <FaTimes /> : <CiMenuFries />}
+          </button>
+
+          {/* Menu (Mobile) */}
+          <div
+            className={`${
+              click ? "block" : "hidden"
+            } lg:hidden absolute top-0 left-0 w-full h-screen bg-[#00009c] flex flex-col items-center justify-center transition-all duration-300 ease-in-out z-40`}
+          >
+            <ul className="text-center text-white text-xl">
+              <Link to="/lista-pedidos">
+                <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:text-black hover:rounded">
+                  Lista de Pedidos
+                </li>
+              </Link>
+              <Link to="/cadastro">
+                <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:text-black hover:rounded">
+                  Cadastro
+                </li>
+              </Link>
+              <Link to="/relatorio">
+                <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:text-black hover:rounded">
+                  Relatório
+                </li>
+              </Link>
+              <Link to="/retirada">
+                <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:text-black hover:rounded">
+                  Retirada
+                </li>
+              </Link>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* Overlay cobrindo o conteúdo da home */}
+      <div className={`${click ? "hidden" : "block"}`}>
+        {/* Conteúdo da Home (Cards) */}
+        <div className="bg-white">
+          <div
+            className="container"
+            style={{
+              maxWidth: "2000px",
+              height: "70vh",
+              margin: "0px auto",
+              textAlign: "center",
+              position: "relative",
+              top: "20vh",
+            }}
+          >
+            <div
+              style={{
+                display: "grid",
+                gridTemplateColumns: "repeat(auto-fit, minmax(250px, 1fr))",
+                gap: "20px",
+                position: "relative",
+                top: "5vh",
+              }}
+            >
+              {/* CARDS */}
+              <div
+                className="card"
+                style={{
+                  background: "#fff",
+                  borderRadius: "8px",
+                  boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+                  padding: "20px",
+                  cursor: "pointer",
+                  transition: "transform 0.3s ease",
+                  marginBottom: "10px",
+                  color: "black",
+                }}
+              >
                 <Link to="/lista-pedidos">
-                  <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:rounded">
-                    Lista de Pedidos
-                  </li>
+                  <img src="/listaPedidos.svg" className="h-[120px] ml-24" />
+                  <h2 className="text-[40px]">Lista de Pedidos</h2>
                 </Link>
+              </div>
+              <div
+                className="card"
+                style={{
+                  background: "#fff",
+                  borderRadius: "8px",
+                  boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+                  padding: "20px",
+                  cursor: "pointer",
+                  transition: "transform 0.3s ease",
+                  marginBottom: "10px",
+                  color: "black",
+                }}
+              >
                 <Link to="/cadastro">
-                  <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:rounded">
-                    Cadastro
-                  </li>
+                  <img src="/cadastro.svg" className="h-[120px] ml-24" />
+                  <h2 className="text-[40px]">Cadastro</h2>
                 </Link>
+              </div>
+              <div
+                className="card"
+                style={{
+                  background: "#fff",
+                  borderRadius: "8px",
+                  boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+                  padding: "20px",
+                  cursor: "pointer",
+                  transition: "transform 0.3s ease",
+                  marginBottom: "10px",
+                  color: "black",
+                }}
+              >
                 <Link to="/relatorio">
-                  <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:rounded">
-                    Relatório
-                  </li>
+                  <img src="/relatorio.svg" className="h-[120px] ml-20" />
+                  <h2 className="text-[40px]">Relatórios</h2>
                 </Link>
+              </div>
+              <div
+                className="card"
+                style={{
+                  background: "#fff",
+                  borderRadius: "8px",
+                  boxShadow: "0 4px 8px rgba(0,0,0,0.1)",
+                  padding: "20px",
+                  cursor: "pointer",
+                  transition: "transform 0.3s ease",
+                  marginBottom: "10px",
+                  color: "black",
+                }}
+              >
                 <Link to="/retirada">
-                  <li className="my-4 py-4 border-b border-[#00FF62] hover:bg-white hover:rounded">
-                    Retirada
-                  </li>
+                  <img src="/retirada.svg" className="h-[120px] ml-20" />
+                  <h2 className="text-[40px]">Retirada</h2>
                 </Link>
-              </ul>
+              </div>
             </div>
-          )}
+          </div>
         </div>
-
-        {/* Botão do Menu (Mobile) */}
-        <button
-          className="block sm:hidden transition text-white text-[30px]"
-          onClick={handleClick}
-        >
-          {click ? <FaTimes /> : <CiMenuFries />}
-        </button>
       </div>
-    </nav>
+    </>
   );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,4 @@
 import Header from "../components/header"
-import { Link } from "react-router-dom";
 
 export default function Home () {
     return(
@@ -9,55 +8,14 @@ export default function Home () {
                 background: "White",
                 fontFamily: "Chakra Petch , sans serif",
                 fontSize: "20px",
-                textAlign: "center"
+                textAlign: "center",
            }}
         >
-    {/* HEADER */}
-      <div className="bg-[#00009c]">
-        <Header />
-      </div>
-        {/* ATALHOS */}
-        <div className="bg-white">
-              {/* Container Responsivo */}
-              <div className="container max-w-[2000px] h-[70vh] mx-auto text-center relative top-[9vh]">
-                {/* Menu com Grid Responsivo */}
-                <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 relative top-[20vh]">
-                  
-                  {/* Card Lista de Pedidos */}
-                  <div className="card bg-white rounded-lg shadow-md p-6 gap-3 cursor-pointer transition-transform duration-300 hover:scale-105">
-                    <Link to={"/lista-pedidos"}>
-                      <img src="/listaPedidos.svg" className="h-[120px] mx-auto" alt="Lista de Pedidos"/>
-                      <h2 className="text-[24px] sm:text-[30px] lg:text-[40px] font-bold mt-4">Lista de Pedidos</h2>
-                    </Link>
-                  </div>
-
-                  {/* Card Cadastro */}
-                  <div className="card bg-white rounded-lg shadow-md p-6 cursor-pointer transition-transform duration-300 hover:scale-105">
-                    <Link to={"/cadastro"}>
-                      <img src="/cadastro.svg" className="h-[120px] mx-auto" alt="Cadastro"/>
-                      <h2 className="text-[24px] sm:text-[30px] lg:text-[40px] font-bold mt-4">Cadastro</h2>
-                    </Link>
-                  </div>
-
-                  {/* Card Relatórios */}
-                  <div className="card bg-white rounded-lg shadow-md p-6 cursor-pointer transition-transform duration-300 hover:scale-105">
-                    <Link to={"/relatorio"}>
-                      <img src="/relatorio.svg" className="h-[120px] mx-auto" alt="Relatórios"/>
-                      <h2 className="text-[24px] sm:text-[30px] lg:text-[40px] font-bold mt-4">Relatórios</h2>
-                    </Link>
-                  </div>
-
-                  {/* Card Retirada */}
-                  <div className="card bg-white rounded-lg shadow-md p-6 cursor-pointer transition-transform duration-300 hover:scale-105">
-                    <Link to={"/retirada"}>
-                      <img src="/retirada.svg" className="h-[120px] mx-auto" alt="Retirada"/>
-                      <h2 className="text-[24px] sm:text-[30px] lg:text-[40px] font-bold mt-4">Retirada</h2>
-                    </Link>
-                  </div>
-                  
-                </div>
+            {/* HEADER */}
+              <div className="bg-[#00009c]">
+                <Header />
               </div>
-            </div>
+
           </div>
         </>
     )
