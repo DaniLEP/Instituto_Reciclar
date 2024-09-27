e23ad900be50458bda349884682aa14244460d5c
DaniLEP <danilomanzoli64@gmail..com>
2024-09-27 10:56:43 -0300

    ft: alteration the home page, header and cards

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ export default function Home () {
                 <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-3 relative top-[5vh]">
                   
                   {/* Card Lista de Pedidos */}
-                  <div className="card bg-white rounded-lg shadow-md p-6 cursor-pointer transition-transform duration-300 hover:scale-105">
+                  <div className="card bg-white rounded-lg shadow-md p-6 gap-3 cursor-pointer transition-transform duration-300 hover:scale-105">
                     <Link to={"/lista-pedidos"}>
                       <img src="/listaPedidos.svg" className="h-[120px] mx-auto" alt="Lista de Pedidos"/>
                       <h2 className="text-[24px] sm:text-[30px] lg:text-[40px] font-bold mt-4">Lista de Pedidos</h2>
