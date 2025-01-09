1af0e2e46cae99b8b1cc0ad8af1315bd92ba066b
DaniLEP <danilomanzoli64@gmail.com>
2025-01-09 15:59:50 -0300

    falta iniciar a hostilização do site

--- src/main.jsx
+++ src/main.jsx
@@ -29,6 +29,7 @@ import Gerenciador from './components/Product Manager/GerenciadorProdutos.jsx'
 import Registro from './pages/User_Registration/Registro_Usuario.jsx'
 import AdminUsuarios from './components/Profile_Verification/Verificação.jsx'
 import VisualizarFornecedores from './components/Supplier_Registration/View_Supplier/VisualizarSupplier.jsx'
+import TabelaRefeicoes from './components/Revenue_Registration/Quantity_Ref/Status_Revenue.jsx'
 // import StatusPedidos from './components/Status_Order/StatusPedido.jsx'
 
 
@@ -103,8 +104,8 @@ const router = createBrowserRouter ([
         element: <CadastroRefeicoes />,
       },
       {
-        path: "/Quantidade_Refeicoes",
-        // element: <QuantidadeRefeicoes />,
+        path: "/Refeicoes_Servidas",
+        element: <TabelaRefeicoes />,
       },
       // PAGINAS DO CARDS DE DASHBOARD
       {
